bd1afb605d6b2b2c92f237d7940b96741d99300a
skykong <9601698@qq.com>
2019-04-18 19:49:55 +0800

    修改

--- skyjt.js
+++ skyjt.js
@@ -1,17 +1,21 @@
 #!/usr/bin/env node
 let commander = require('commander')
 let $ = require('meeko')
-let Pack = require('package.json')
+let Pack = require('./package.json')
 commander
   .usage('[options] ')
   .version(`[${$.c.g(Pack.version)}] Sky Framework`, '-v, --version')
   // .option('-a, --aaa-bbb', 'commander.aaaBbb')
-
   .command('init [path]')
   .alias('i')
   .description('Init Sky framework')
   .action(function (env, options) {
     console.log('do init')
+  })
+  .command('dbscan -c config.json')
+  .description('scan DB')
+  .action(function (env, options) {
+    console.log('scan DB')
   })
 
 commander.parse(process.argv)
