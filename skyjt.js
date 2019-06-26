5cc12c51cd0d05f6e548955e2d61baae62c9aff1
skykong <9601698@qq.com>
2019-06-26 10:37:29 +0800

    :memo:

--- skyjt.js
+++ skyjt.js
@@ -22,7 +22,7 @@ async function checkVersion () {
 checkVersion()
 let tools = $.requireAll(path.join(__dirname, '.', 'lib'))
 let spinnerHandler = {}
-// 输出字符键盘
+// 输出字符键盘1
 function keyboard () {
   console.log((_ => [..."`1234567890-=~~QWERTYUIOP[]\\~ASDFGHJKL;'~~ZXCVBNM,./~"].map(x => (o += `/${b = '_'.repeat(w = x < y ? 2 : ' 667699'[x = ['BS', 'TAB', 'CAPS', 'ENTER'][p++] || 'SHIFT', p])}\\|`, m += y + (x + '    ').slice(0, w) + y + y, n += y + b + y + y, l += ' __' + b)[73] && (k.push(l, m, n, o), l = '', m = n = o = y), m = n = o = y = '|', p = l = k = []) && k.join`
 `)())
